70651853624d7d6940b8895351bfa2edd6a5880a
ValerioGc <valerio.gc1@gmail.com>
2022-08-22 16:12:03 +0200

    Avanzamento

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 var app = new Vue({
     el:'#app',
+    data : {
+        currEmail: '',
+        emailArray: []
+    },
+    created() {
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    this.currEmail = response.data.response;
+                    this.emailArray.push(this.currEmail);
+                })
+        }
+        console.table(this.emailArray)
+    }
 
+    
 })
+
+
+
+
